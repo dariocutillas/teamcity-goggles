be9326b5c842fcb49d27059c8c49050bbfee4782
Darío Cutillas <dario.cutillas@neotechnology.com>
2020-02-12 07:42:30 +0100

    Add limit and delimiter otions

--- tc_goggles/examples/find_parameter.py
+++ tc_goggles/examples/find_parameter.py
@@ -1,13 +1,18 @@
 import os
 import re
 import itertools
+from collections import namedtuple
 import argparse
+import sys
 
 from tc_goggles.parameter_filters import name_matches, value_matches, inherited
 from tc_goggles.endpoint import ServerConfig, TeamCityEndPoint, Auth
 
 
-def find_params(parameter_filter):
+Result = namedtuple('Result', 'build_type,parameter')
+
+
+def find_params(parameter_filter, limit):
     server_config = ServerConfig(
         # https server address
         uri = os.environ["TEAMCITY_SERVER"],
@@ -20,19 +25,19 @@ def find_params(parameter_filter):
     build_types = itertools.chain(tc.build_configurations(), tc.build_templates())
 
     # Get the parameter and build_type constrained to the filter
-    params = ((parameter, build_type) \
+    params = (Result(build_type, parameter) \
                 for build_type in build_types \
                 for parameter in build_type.parameters() if parameter_filter(parameter))
 
-    for (param, build_type) in params:
-        msg = f'{build_type.web_url}  {param.name}  {param.value}'
-        print(msg)
+    return itertools.islice(params, limit)
         
 
 def main():
     parser = argparse.ArgumentParser(description='Find build types containing parameters with specific values.')
     parser.add_argument('name', type=str, help='The name of the parameter.')
     parser.add_argument('--values', type=str, metavar='VALUE', nargs='+', help='Optional list of values the parameter should have.')
+    parser.add_argument('--limit', type=int, default=sys.maxsize, help='Limit the number of results')
+    parser.add_argument('--delimiter', type=str, default='  ', help='Field delimiter. 2 spaces by default.')
 
     args = parser.parse_args()
 
@@ -47,7 +52,9 @@ def main():
         regex = f"({'|'.join(args.values)})" 
         parameter_filter = parameter_filter & value_matches(regex)
      
-    find_params(parameter_filter)
+    for result in find_params(parameter_filter, args.limit):
+        msg = f'{result.build_type.web_url}{args.delimiter}{result.parameter.name}{args.delimiter}{result.parameter.value}'
+        print(msg)
     
 if __name__ == '__main__':
     main()
